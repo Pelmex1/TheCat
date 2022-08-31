df509c5f46ee54b14274c3e01f776f4b48c8deb6
Pelmex1 <89773802+Pelmex1@users.noreply.github.com>
2022-08-31 10:09:42 +0300

    add ua

--- slash/user_info.js
+++ slash/user_info.js
@@ -3,4 +3,5 @@ const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.j
 module.exports = new ContextMenuCommandBuilder()
 	.setName('User info')
     .setNameLocalization('ru', 'Информация о пользователе')
+    .setNameLocalization('ua', 'Дані про користувача')
 	.setType(ApplicationCommandType.User);
