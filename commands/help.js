b5fdcd9ba71e473e2e89fa520e845222013598df
Pelmex1 <89773802+Pelmex1@users.noreply.github.com>
2022-09-04 14:08:14 +0300

    поправка

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports = async () => {
                          .setColor('2617db')
                          .setTitle( `${codes.rules}`)
                          .setThumbnail(bot.user.id)
-                         .setDescription(` ${codes.help-help} \n ${codes.avatar-help} \n ${codes.info-help} \n ${codes.InfoUser-help} \n ${codes.AutoRoleJoin-help} \n ${codes.settingsViev-help} \n 
+                         .setDescription(` ${codes.help-help} \n ${codes.avatar-help} \n ${codes.info-help} \n ${codes.SettingsAutoRoleJoin-help} \n ${codes.settingsViev-help} \n 
                          ${codes.settingsLang-help} \n ${codes.settingsAutoRoleButtonsAdd-help} \n ${codes.settingsAutoRoleButtonsDelete-help} \n ${codes.settingsVerificatinSwitch-help} \n 
                          ${codes.settingsVerificationLength-help} \n ${codes.settingsVerificationSand-help}`)
                          ] });
